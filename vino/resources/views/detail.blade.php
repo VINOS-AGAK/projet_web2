fcdbdaa06f25100165ec7ee8a07554ac217b0849
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-07 22:43:58 -0500

    creation css/detail/detail.css et ajoute style pour detail.blade

--- vino/resources/views/detail.blade.php
+++ vino/resources/views/detail.blade.php
@@ -1,28 +1,27 @@
-<!-- <p>Page detail</p>
-создать страницу товара (вино) с фотографией слева и описанием справа (название, год сбора урожая, формат, страна, описание, дата приобретения, цена, количество  ) для мобильного экрана (HTML, CSS) и потом адаптивная для большого экрана где должна занимать  весь экран -->
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Название товара</title>
+    <title>Detail vin</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head> 
   <body>
     <div class="product-container">
       <div class="product-image">
-        <img src="{{ asset('img/vin2.png')}}" alt="Название товара" />
+        <img src="{{ asset('img/vin2.png')}}" alt="nom vin" />
       </div>
       <div class="product-description">
-        <h1>Название товара</h1>
+        <h1>Nom</h1>
         <ul>
-          <li>Год сбора урожая: 2020</li>
-          <li>Формат: 750 мл</li>
-          <li>Страна: Италия</li>
-          <li>Описание: Вкусное красное вино из солнечной Италии</li>
-          <li>Дата приобретения: 1 марта 2023 г.</li>
-          <li>Цена: $20</li>
-          <li>Количество: 10</li>
+          <li>Millesime: 2020</li>
+          <li>Format: 750 ml</li>
+          <li>Pays: Itali</li>
+          <li>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat numquam qui</li>
+          <li>Date d'achat: 1 mars 2023 </li>
+          <li>Prix: $20</li>
+          <li>Quantite: 10</li>
         </ul>
       </div>
     </div>

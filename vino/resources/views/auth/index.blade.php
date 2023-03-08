193198b83b40efff637d30bf6a88f80249a138da
Alexandru Candu <102234554+TITAN-85@users.noreply.github.com>
2023-03-08 17:46:19 -0500

    Creation des bottons Log in Logout, netoyage, arangement de code

--- vino/resources/views/auth/index.blade.php
+++ vino/resources/views/auth/index.blade.php
@@ -1,64 +1,56 @@
 @extends('layouts.app')
 @section('title', 'Sign in')
-@section('content') 
+@section('content')
 
-<main class="login-form">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 pt-4">
-                <div class="card">
-                    <div class="card-body">
+<main class="">
+    <div class="">
 
-                        @if(session('success'))
-                        <div class="">
-                            {{session('success')}}
-                        </div>
-                        @endif
-
-                        @if(!$errors->isEmpty())
-                        <div class="" role="alert">
-                            <ul>
-                                @foreach($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                            <button type="button" class="" aria-label="Close"></button>
-                        </div>
-                        @endif
-
-                        <form action="{{route('user.auth')}}" method="post">
-                            @csrf
-
-                            <div class="">
-                                <input type="email" placeholder="email" class="form-control" name="email" value="email">
-                                
-                                @if($errors->has('email'))
-                                <div class="text-danger mt-2">
-                                    {{$errors->first('email')}}
-                                </div>
-                                @endif
-
-                            </div>
-                            <div class="">
-                                <input type="password" placeholder="password" class="form-control" name="password">
-
-                                @if($errors->has('password'))
-                                <div class="">
-                                    {{$errors->first('password')}}
-                                </div>
-                                @endif
-
-                            </div>
-                            <div class="">
-                                <input type="submit" value="submit" class="">
-                            </div>
-                        </form>
-                        <button><a href="#">Forgot password btn no route yet</a></button>
-                    </div>
-                </div>
-            </div>
+        @if(session('success'))
+        <div class="">
+            {{session('success')}}
         </div>
+        @endif
+
+        @if(!$errors->isEmpty())
+        <div class="" role="alert">
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="" aria-label="Close"></button>
+        </div>
+        @endif
+
+        <form action="{{route('user.auth')}}" method="post">
+            @csrf
+
+            <div class="">
+                <input type="email" placeholder="email" class="form-control" name="email" value="email">
+
+                @if($errors->has('email'))
+                <div class="text-danger mt-2">
+                    {{$errors->first('email')}}
+                </div>
+                @endif
+
+            </div>
+            <div class="">
+                <input type="password" placeholder="password" class="form-control" name="password">
+
+                @if($errors->has('password'))
+                <div class="">
+                    {{$errors->first('password')}}
+                </div>
+                @endif
+
+            </div>
+            <div class="">
+                <input type="submit" value="submit" class="">
+            </div>
+        </form>
+        <button><a href="#">Forgot password btn no route yet</a></button>
     </div>
 </main>
 
-{{-- @endsection --}}
+@endsection

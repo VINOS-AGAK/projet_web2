6c13921adbbbff3eabe00b8ab6b8e6cfb2440f11
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-10 22:57:03 -0500

    add

--- vino/resources/views/cellier/index.blade.php
+++ vino/resources/views/cellier/index.blade.php
@@ -85,6 +85,22 @@
         </div>
       </div>
     </div>
+    <div class="card">
+      <img src="{{ asset('img/vin2.png')}}" alt="Product Image">
+      <picture class="modal"><img src="{{ asset('img/vin2.png')}}" alt="img"></picture>
+      <div class="card-info">
+        <div class="card-info-title">
+
+          <h3 class="card-title">Riesling Pinot gris Mosel</h3>
+          <p class="card-subtitle">White wine 750 ml</p>
+        </div>
+        <div class="card-info-client">
+
+          <p class="card-count">Quantite:: 10 </p>
+          <p class="card-rating">Note: &#9733;&#9733;&#9733;&#10025;</p>
+        </div>
+      </div>
+    </div>
   </div>
 
 </div>

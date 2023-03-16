0d6119fba11a03fd82bf4b2eb2ae42aea795e214
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-16 15:48:10 -0400

    add dans router.js les routes catalogue et bouteille-cellier

--- vino/resources/js/router.js
+++ vino/resources/js/router.js
@@ -7,6 +7,8 @@ Vue.use(VueRouter);
 import Welcome from "./views/Welcome";
 import cellier from "./views/Cellier";
 import login from "./views/auth/Index";
+import Catalogue from "./views/catalogue/Catalogue";
+import BouteilleCellier from "./views/cellier/BouteilleCellier";
 
 const routes = [
     {
@@ -20,6 +22,14 @@ const routes = [
     {
         path: "/login",
         component: login
+    },
+    {
+        path: "/catalogue",
+        component: Catalogue
+    },
+    {
+        path: "/bouteille-cellier",
+        component: BouteilleCellier
     }
 ]
 

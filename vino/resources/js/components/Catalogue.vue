79cb7f31e29231dc247f8dd87036a6726bd7aa98
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-22 23:48:33 -0400

    installer axios et ajouter sur script + barre de recherche dans catalogue

--- vino/resources/js/components/Catalogue.vue
+++ vino/resources/js/components/Catalogue.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 
     <div class="product-grid" >
       <div class="product-card" v-for="bouteille in catalogue" :key="bouteille.id">
@@ -12,17 +12,11 @@
         </router-link>
         <br>
         <a href="#" @click.prevent="deleteCatalogue(bouteille.id)">Supprimer</a>
+      </div>
     </div>
-      
-      
-    
-    
-    </div>
-    
     </template>
     
     <script>
-    
     import useCatalogue from '../composables/catalogue'
     import { onMounted } from 'vue'
         export default{
@@ -30,14 +24,63 @@
             setup(){
                 const { catalogue, getCatalogue, deleteCatalogue } = useCatalogue()
                 onMounted(getCatalogue)
-    
                 return { 
                     catalogue, 
                     getCatalogue, 
                     deleteCatalogue 
                 }
             }
-    
         }
-    </script>
+    </script> -->
+    <template>
+        <div>
+            <input type="text" v-model="searchTerm" autocomplete="off" list="catalogue-names">
+    <datalist id="catalogue-names">
+      <option v-for="bouteille in catalogue" :value="bouteille.nom">{{ bouteille.nom }}</option>
+    </datalist>
+          <div class="product-grid">
+            <div class="product-card" v-for="bouteille in filteredCatalogue" :key="bouteille.id">
+              <!-- Код для отображения товара -->
+              <img :src="bouteille.image" alt="Product 1">
+        <h3>{{bouteille.nom}}</h3>
+        <p>{{bouteille.description}}</p>
+        <p>{{bouteille.pays}}</p>
+        <p>{{bouteille.format}}</p>
+        <router-link :to="{name: 'catalogue.edit', params:{ id: bouteille.id } }">
+            Modifier
+        </router-link>
+        <br>
+        <a href="#" @click.prevent="deleteCatalogue(bouteille.id)">Supprimer</a>
+            </div>
+          </div>
+        </div>
+      </template>
+      
+      <script>
+      import useCatalogue from '../composables/catalogue'
+      import { onMounted, ref, computed } from 'vue'
+      
+      export default {
+        setup() {
+          const { catalogue, getCatalogue, deleteCatalogue } = useCatalogue()
+          const searchTerm = ref('')
+          onMounted(getCatalogue)
+      
+          const filteredCatalogue = computed(() => {
+            return catalogue.value.filter(bouteille => {
+              return bouteille.nom.toLowerCase().includes(searchTerm.value.toLowerCase())
+            })
+          })
+      
+          return {
+            catalogue,
+            getCatalogue,
+            deleteCatalogue,
+            searchTerm,
+            filteredCatalogue
+          }
+        }
+      }
+      </script>
+      
     

4d0da4ea5dbb2cada33a4ad40a69730bd8c020c1
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-25 11:30:31 -0400

    style catalogue-card dans nouveau vue modifier

--- vino/resources/js/components/Catalogue.vue
+++ vino/resources/js/components/Catalogue.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <header class="site-header">
       <form class="search" action="#" method="GET">
@@ -86,14 +86,16 @@ export default {
     }
   }
 }
-</script>
+</script> -->
 
- <!-- <template>
+ <template>
   <div>
     <header class="site-header">
       <form class="search" action="#" method="GET">
         <input type="search" v-model="searchTerm" autocomplete="off" list="catalogue-names" @change="selectProduct" @input="selectedProduct = null">
-        <button type="submit" class="search-button"></button>
+        <button type="submit" class="search-button"><svg xmlns="http://www.w3.org/2000/svg" height="24" width="24">
+        <path d="m19.45 21.325-6.3-6.3q-.725.55-1.675.85-.95.3-2.05.3-2.775 0-4.712-1.937Q2.775 12.3 2.775 9.525q0-2.775 1.938-4.713Q6.65 2.875 9.425 2.875q2.775 0 4.712 1.937 1.938 1.938 1.938 4.713 0 1.1-.313 2.05-.312.95-.837 1.65l6.325 6.325ZM9.425 13.65q1.725 0 2.925-1.2 1.2-1.2 1.2-2.925 0-1.725-1.2-2.925-1.2-1.2-2.925-1.2Q7.7 5.4 6.5 6.6 5.3 7.8 5.3 9.525q0 1.725 1.2 2.925 1.2 1.2 2.925 1.2Z" />
+      </svg></button>
       </form>
     </header>
 
@@ -101,27 +103,26 @@ export default {
       <option v-for="bouteille in filteredCatalogue.slice(0, 6)" :value="bouteille.nom">{{ bouteille.nom }}</option>
     </datalist>
 
-    <section class="catalogue-container">
-      <article v-if="selectedProduct" class="product-card">
-        <div class="card-body">
+    <section class="">
+      <article v-if="selectedProduct" class="catalogue__card">
+        <div class="catalogue__card-body">
           <img :src="selectedProduct.image" alt="img-bouteille">
           <picture class="modal"><img :src="selectedProduct.image" alt="img-bouteille"></picture>
-          <div class="card-info">
+          <div class="catalogue__card-info">
             <div class="card-info-title">
               <h3 class="card-title">{{ selectedProduct.nom }}</h3>
               <p class="card-subtitle">{{ selectedProduct.description }} {{ selectedProduct.format }}</p>
               <p class="card-subtitle">{{ selectedProduct.pays }}</p>
             </div>
             <div class="card-info-client">
-              <p class="card-count">{{ selectedProduct.prix_saq }}$</p>
-              <div class="card-footer">
-                <button class="btn" value="">Buy Now</button>
-                <router-link :to="{name: 'catalogue.edit', params:{ id: selectedProduct.id } }">
-                  Modifier
+              <p class="catalogue__card-count">{{ selectedProduct.prix_saq }}$</p>
+             
+                <router-link class="btn" :to="{name: 'catalogue.edit', params:{ id: selectedProduct.id } }">
+                  Acheter
                 </router-link>
                 <br>
-                <a href="#" @click.prevent="deleteCatalogue(selectedProduct.id)" class="btn">Supprimer</a>
-              </div>
+                <!-- <a href="#" @click.prevent="deleteCatalogue(selectedProduct.id)" class="btn">Supprimer</a> -->
+             
             </div>
           </div>
         </div>
@@ -166,7 +167,7 @@ export default {
       }
     }
   }
-  </script>  -->
+  </script> 
    
     <!-- <template>
 

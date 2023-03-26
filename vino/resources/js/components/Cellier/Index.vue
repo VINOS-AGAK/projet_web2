b39341a2f4d97e31dce9a769cac976690df344bf
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-26 00:49:06 -0400

    ajout style cellier.css et button add-cellier

--- vino/resources/js/components/Cellier/Index.vue
+++ vino/resources/js/components/Cellier/Index.vue
@@ -1,34 +1,34 @@
 <template>
+    <!-- Pour pluseur cellier TODO: ( Later ) -->
     <section>
-        <h2 class="liste__titre" >Mes cellier</h2>
-        <article class="container">
+    <div class="ajouter-cellier">
+        <a href="#" class="ajouter-cellier__bouton">Ajouter un nouveau cellier</a>
+    </div>
+    <h2 class="liste__titre">Mes cellier</h2>
+    <article class="container">
+        <div class="card" v-for="unCellier in mesCellier" :key="unCellier.id">
+            <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
+                <p>Nom: {{ unCellier.nom }}</p>
+                <p>Description : {{ unCellier.description }}</p>
+            </router-link>
+        </div>
+        <div v-if="mesCellier.length === 0">
+            <p class="text-danger">Vous n'avez pas de cellier. Voulez-vous en créer un? Cliquez <a href="#">ici</a></p>
+        </div>
+    </article>
+</section>
 
-            <div class="card" v-for="unCellier in mesCellier" :key="unCellier.id">
-                <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
-                    <p>Nom: {{ unCellier.nom }}</p>
-                    <p>Description : {{ unCellier.description }}</p>
-                </router-link>
-            </div>
-            <div   v-if="mesCellier.length === 0">
-                
-                    <p class="text-danger">Vous n'avez pas de cellier. Voulez-vous en créer un? Cliquez <a href="#">ici</a> </p>
-                
-            </div>
-        </article>
+    <!-- <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
+        <div class="product-card">
+            <p>Cellier: {{ unCellier.nom }}</p>
+            <p>Description: {{ unCellier.description }}</p>
+            <h5>Voire mes bouteilles</h5>
+        </div>
+        <br>
+    </router-link> -->
 
-        <!-- <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
-            <div class="product-card">
-                <p>Cellier: {{ unCellier.nom }}</p>
-                <p>Description: {{ unCellier.description }}</p>
-                <h5>Voire mes bouteilles</h5>
-            </div>
-            <br>
-        </router-link> -->
+    <!-- <a href="#" @click.prevent="deleteCatalogue(bouteille.id)">Supprimer</a> -->
 
-        <!-- <a href="#" @click.prevent="deleteCatalogue(bouteille.id)">Supprimer</a> -->
-
-        <!-- Pour pluseur cellier TODO: ( Later ) -->
-    </section>
 </template>
 
 <script>

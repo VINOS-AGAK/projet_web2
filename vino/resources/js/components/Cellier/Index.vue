964dfbc2ad6322eb58048c055e2dbd96f657d47a
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-03-26 14:09:49 -0400

    modif img

--- vino/resources/js/components/Cellier/Index.vue
+++ vino/resources/js/components/Cellier/Index.vue
@@ -10,7 +10,7 @@
             
 
                 <div class="card-body cellier__card-body">
-                    <img src="https://media.istockphoto.com/id/471642528/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B0%D0%BA%D0%B2%D0%B0%D1%80%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%B1%D0%BE%D0%BA%D0%B0%D0%BB-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B3%D0%BE-%D0%B2%D0%B8%D0%BD%D0%B0.jpg?s=1024x1024&w=is&k=20&c=sb0epl26InB9CDERl8RjaSRaqV8m_XDcA8qKJDw0VsE="  alt="img-bouteille">
+                    <img src="https://as2.ftcdn.net/v2/jpg/00/97/95/11/1000_F_97951154_mc0qsUpZBWWNm1TjSehhwCQJ4lBiRGm4.jpg"  alt="img-bouteille">
                     <div class="cellier-info">
                         <div class="card-info-title">
                             <h3 class="cellier__card-title"><router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">{{ unCellier.nom }}</router-link></h3>
@@ -36,7 +36,8 @@
             <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
 
                 <div class="card-body cellier__card-body">
-                    <img src="https://media.istockphoto.com/id/471642528/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B0%D0%BA%D0%B2%D0%B0%D1%80%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%B1%D0%BE%D0%BA%D0%B0%D0%BB-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B3%D0%BE-%D0%B2%D0%B8%D0%BD%D0%B0.jpg?s=1024x1024&w=is&k=20&c=sb0epl26InB9CDERl8RjaSRaqV8m_XDcA8qKJDw0VsE="  alt="img-bouteille">
+                    <img src="https://as2.ftcdn.net/v2/jpg/00/97/95/11/1000_F_97951154_mc0qsUpZBWWNm1TjSehhwCQJ4lBiRGm4.jpg"  alt="img-bouteille">
+                    <!-- <img src="https://media.istockphoto.com/id/471642528/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B0%D0%BA%D0%B2%D0%B0%D1%80%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%B1%D0%BE%D0%BA%D0%B0%D0%BB-%D0%BA%D1%80%D0%B0%D1%81%D0%BD%D0%BE%D0%B3%D0%BE-%D0%B2%D0%B8%D0%BD%D0%B0.jpg?s=1024x1024&w=is&k=20&c=sb0epl26InB9CDERl8RjaSRaqV8m_XDcA8qKJDw0VsE="  alt="img-bouteille"> -->
                     <div class="cellier-info">
                         <div class="card-info-title">
                             <h3 class="cellier__card-title">{{ unCellier.nom }}</h3>

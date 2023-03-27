35a82cad8c59a84b3572b16e78b63688dacb20ec
TITAN-85 <102234554+TITAN-85@users.noreply.github.com>
2023-03-26 20:10:38 -0400

    Cree cellier/ Effacer cellier - fonction

--- vino/resources/js/components/Cellier/Index.vue
+++ vino/resources/js/components/Cellier/Index.vue
@@ -28,14 +28,14 @@
                             <!-- <router-link :to="{ name: 'cellier.edit', params: { id: selectedProduct.id } }">
                                 Modifier
                                 </router-link> -->
-                            <router-link class="card-btn_modif " value="" :to="{}">Modifier</router-link>
-                            <button class="card-btn_supp deleteModalBtn" value="">-</button>
+                            <router-link class="card-btn_modif " value="" :to="{ name: 'cellier.edit', params: { id: unCellier.id }}">Modifier</router-link>
+                            <button class="card-btn_supp deleteModalBtn" @click.prevent="deleteCellier(unCellier.id)">X</button>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="card" v-for="unCellier in mesCellier" :key="unCellier.id">
+            <!-- <div class="card" v-for="unCellier in mesCellier" :key="unCellier.id">
                 <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
 
                     <div class="card-body cellier__card-body">
@@ -51,7 +51,7 @@
                         </div>
                     </div>
                 </router-link>
-            </div>
+            </div> -->
 
             <div v-if="mesCellier.length === 0">
                 <p class="text-danger">Vous n'avez pas de cellier. Voulez-vous en créer un? Cliquez <router-link
@@ -60,17 +60,6 @@
 
         </article>
     </section>
-
-    <!-- <router-link :to="{ name: 'bouteille.index', params: { id: unCellier.id } }">
-        <div class="product-card">
-            <p>Cellier: {{ unCellier.nom }}</p>
-            <p>Description: {{ unCellier.description }}</p>
-            <h5>Voire mes bouteilles</h5>
-        </div>
-        <br>
-    </router-link> -->
-
-    <!-- <a href="#" @click.prevent="deleteCatalogue(bouteille.id)">Supprimer</a> -->
 </template>
 
 <script>

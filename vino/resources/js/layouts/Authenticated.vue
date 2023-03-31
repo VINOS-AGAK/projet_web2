57b21ffeb5896cabf00cd4d2df96bea3827ca565
Gabi-Tho <e2194469@cmaisonneuve.qc.ca>
2023-03-31 12:42:24 -0400

    nettoyage de html dans authenticated.vue

--- vino/resources/js/layouts/Authenticated.vue
+++ vino/resources/js/layouts/Authenticated.vue
@@ -5,52 +5,51 @@
             <div class="site-header-content">
                 <ul class="menu">
                             <!-- Logo -->
-                            <li class="menu-item">
-                                <a href="/Accueil">
-                                    <IconContainer>
-                                        <template #icon>
-                                            <IconLogo/>
-                                        </template>
-                                    </IconContainer>
-                                </a>
-                            </li>
+                    <li class="menu-item">
+                        <a href="/Accueil">
+                            <IconContainer>
+                                <template #icon>
+                                    <IconLogo/>
+                                </template>
+                            </IconContainer>
+                        </a>
+                    </li>
 
-                            <li class="menu-item">
-                                <router-link :to="{ name: 'catalogue.index' }" active-class="border-b-2 border-indigo-400" class="">
-                                    <IconContainer>
-                                        <template #icon>
-                                            <IconBouteille/>
-                                        </template>
-                                    </IconContainer>
-                                </router-link>
-                            </li>
-                            <li class="menu-item">
-                                <router-link :to="{ name: 'cellier.index' }" active-class="border-b-2 border-indigo-400" class="">
-                                    <IconContainer>
-                                        <template #icon>
-                                            <IconCellier/>
-                                        </template>
-                                    </IconContainer>
-                            </router-link>
+                    <li class="menu-item">
+                        <router-link :to="{ name: 'catalogue.index' }" active-class="border-b-2 border-indigo-400" class="">
+                            <IconContainer>
+                                <template #icon>
+                                    <IconBouteille/>
+                                </template>
+                            </IconContainer>
+                        </router-link>
+                    </li>
 
-                            </li>
+                    <li class="menu-item">
+                        <router-link :to="{ name: 'cellier.index' }" active-class="border-b-2 border-indigo-400" class="">
+                            <IconContainer>
+                                <template #icon>
+                                    <IconCellier/>
+                                </template>
+                            </IconContainer>
+                        </router-link>
+                    </li>
 
-                            <li class="menu-item">
-                                <div class="menu-username">{{ user.name }}</div>
-                                    <button @click="logout" type="button" >
-                                        <IconContainer>
-                                            <template #icon>
-                                                <IconProfil/>
-                                            </template>
-                                         </IconContainer>
-                                    </button>
-                            </li>
-                        </ul>
-
-                    </div>
-
-                </div>
-        </nav>
+                    <li class="menu-item">
+                        <div class="menu-username">{{ user.name }}</div>
+                        <button @click="logout" type="button" >
+                            <IconContainer>
+                                <template #icon>
+                                    <IconProfil/>
+                                </template>
+                             </IconContainer>
+                        </button>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <!-- Page Content -->
     <main>

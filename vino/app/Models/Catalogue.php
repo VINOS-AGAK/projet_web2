9a90c20d7d0b78854e9134fc87ad0458dd70c64c
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-04-03 14:23:31 -0400

    ajoute les commentaires selon le standart PHPDoc  pour modeles/catalogue.php modele/cellier et modele/user

--- vino/app/Models/Catalogue.php
+++ vino/app/Models/Catalogue.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Classe Catalogue
+ *
+ * @package App\Models
+ * @property int $id Identifiant du vin dans le catalogue
+ * @property int $vino__type_id Identifiant du type de vin dans le catalogue Vino
+ * @property string $nom Nom du vin
+ * @property string|null $image URL de l'image du vin
+ * @property string|null $code_saq Code SAQ du vin
+ * @property string|null $pays Pays d'origine du vin
+ * @property string|null $description Description du vin
+ * @property float|null $prix_saq Prix du vin à la SAQ
+ * @property string|null $url_saq URL de la fiche SAQ du vin
+ * @property string|null $url_img URL de l'image du vin sur le site SAQ
+ * @property string|null $format Format de la bouteille (750 ml, magnum, etc.)
+ */
 class Catalogue extends Model
 {
     const TABLE = 'vino__bouteille';
@@ -13,6 +29,11 @@ class Catalogue extends Model
 
     use HasFactory;
 
+     /**
+     * Les attributs qui sont assignables en masse.
+     *
+     * @var array
+     */
     protected $fillable = [
         'vino__type_id',
         'nom',

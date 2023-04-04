48433b54e85bee9ce1736aa92b08732d631958dc
Anna <102247563+AnnaSonnova@users.noreply.github.com>
2023-04-04 12:02:10 -0400

    ajoute les commentaires pertitants pour composable/bouteille.js et catalogue.js

--- vino/resources/js/composables/catalogue.js
+++ vino/resources/js/composables/catalogue.js
@@ -1,17 +1,24 @@
-
+// Importation des fonctions de Vue.js et de Vue Router
 import { ref, inject } from 'vue';
 import { useRouter} from 'vue-router';
 
+// Fonction qui permet d'utiliser le catalogue
 export default function useCatalogue() {
 
+    // Déclaration des variables qui contiendront les données du catalogue
     const catalogue = ref([]);
     const oneCatalogue = ref({});
     const router = useRouter();
     const validationErrors = ref ({});
     const isLoading = ref(false);
+
+    // Injection de la bibliothèque SweetAlert
     const swal = inject('$swal');
+
+    // Importation de la bibliothèque axios pour effectuer les requêtes HTTP
     const axios = require('axios');
 
+    // Fonction qui permet de récupérer tous les éléments du catalogue
     const getCatalogue = async () => {
         axios.get('api/catalogue')
         .then(response=>{
@@ -21,6 +28,7 @@ export default function useCatalogue() {
         })
     }
     
+    // Fonction qui permet de récupérer un élément du catalogue en fonction de son ID
     const getOneCatalogue = async (id) => {
         axios.get('api/catalogue/' + id )
         .then(response=>{
@@ -30,6 +38,7 @@ export default function useCatalogue() {
         })
     } 
 
+    // Fonction qui permet de sauvegarder un élément du catalogue
     const storeCatalogue = async (catalogue) => { 
         if(isLoading.value) return;
 
@@ -52,6 +61,7 @@ export default function useCatalogue() {
         .finally(() => isLoading.value = false)
     }
 
+    // Fonction qui permet de mettre à jour un élément du catalogue
     const updateCatalogue = async (catalogue) => { 
         if(isLoading.value) return;
 
@@ -74,6 +84,7 @@ export default function useCatalogue() {
         .finally(() => isLoading.value = false)
     }
 
+    // Fonction qui permet de supprimer un élément du catalogue
     const deleteCatalogue = async (id) => { 
         swal({
             title: 'Are you sure?',
@@ -109,7 +120,7 @@ export default function useCatalogue() {
 
     }
 
-
+    // Retourne les variables et fonctions nécessaires
     return { catalogue, 
             oneCatalogue, 
             getCatalogue, 

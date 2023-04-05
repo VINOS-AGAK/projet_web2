0f31de6377a38937653cb67295e655026c9e4e15
Gabi-Tho <e2194469@cmaisonneuve.qc.ca>
2023-04-05 17:43:48 -0400

    ajustement de celler.js

--- vino/resources/js/components/Bouteille/Index.vue
+++ vino/resources/js/components/Bouteille/Index.vue
@@ -18,21 +18,19 @@
             <div class="card" v-for="bouteille in oneCellier" :key="bouteille.id" >
                 <div class="card-body">
                     <img :src="bouteille.image" :alt="bouteille.nom">
-                    <section class="">
-
+                    <section>
                             <h3 class="card-title">{{ bouteille.nom }}</h3>
                             <p class="card-subtitle">{{ bouteille.description }} </p>
                             
                             <p class="card-count"> {{ bouteille.prix_saq }} $</p>
-                            <p class="card-count"> {{ bouteille.quantite }} bouteille</p>
-                        <div>
+
                             <div class="stars-flex">
                                 <legend></legend>
                                 <form @submit.prevent='changeNote(bouteille)'>
                                     <fieldset>
                                         <button class="stars-btn">
                                             <input class="radio" v-model="bouteille.notes" type="radio" value="1" :id="'star'+bouteille.id+1" name="rating" />
-                                            <label :for="'star'+bouteille.id+1">1</label>
+                                            <label :for="'star'+bouteille.id+1"></label>
                                         </button>
                                     </fieldset>
                                 </form>
@@ -41,7 +39,7 @@
                                     <fieldset>
                                         <button class="stars-btn">
                                             <input class="radio" v-model="bouteille.notes" type="radio" value="2" :id="'star'+bouteille.id+2" name="rating" />
-                                            <label :for="'star'+bouteille.id+2" >2</label>
+                                            <label :for="'star'+bouteille.id+2" ></label>
                                         </button>
                                     </fieldset>
                                 </form>
@@ -50,7 +48,7 @@
                                     <fieldset>
                                         <button class="stars-btn">
                                             <input class="radio" v-model="bouteille.notes" type="radio" value="3" :id="'star'+bouteille.id+3" name="rating" />
-                                            <label :for="'star'+bouteille.id+3" >3</label>
+                                            <label :for="'star'+bouteille.id+3" ></label>
                                         </button>
                                     </fieldset>
                                 </form>
@@ -59,7 +57,7 @@
                                     <fieldset>
                                         <button class="stars-btn">
                                             <input class="radio" v-model="bouteille.notes" type="radio" value="4" :id="'star'+bouteille.id+4" name="rating" />
-                                            <label :for="'star'+bouteille.id+4" >4</label>
+                                            <label :for="'star'+bouteille.id+4" ></label>
                                         </button>
                                     </fieldset>
                                 </form>
@@ -68,20 +66,30 @@
                                     <fieldset>
                                         <button class="stars-btn">
                                             <input class="radio" v-model="bouteille.notes" type="radio" value="5" :id="'star'+bouteille.id+5" name="rating" />
-                                            <label :for="'star'+bouteille.id+5" >5</label>
+                                            <label :for="'star'+bouteille.id+5" ></label>
                                         </button>
                                     </fieldset>
                                 </form>
                             </div>
-                        </div>
 
-                        <div class="card-footer">
-                              <button  class="card-btn_add " @click.prevent="increment(bouteille.id)" >+</button>    
-                              <button  class="card-btn_supp " @click.prevent="decrement(bouteille.id)" :disabled="bouteille.quantite < 1">-</button>   
-                              <button  class="card-btn_delete deleteModalBtn" value="" @click.prevent="deleteBouteille(bouteille.id)" :disabled="bouteille.quantite > 1">
-                                <svg width="10pt" height="10pt" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 105.7 122.88"><path d="M30.46,14.57V5.22A5.18,5.18,0,0,1,32,1.55v0A5.19,5.19,0,0,1,35.68,0H70a5.22,5.22,0,0,1,3.67,1.53l0,0a5.22,5.22,0,0,1,1.53,3.67v9.35h27.08a3.36,3.36,0,0,1,3.38,3.37V29.58A3.38,3.38,0,0,1,102.32,33H98.51l-8.3,87.22a3,3,0,0,1-2.95,2.69H18.43a3,3,0,0,1-3-2.95L7.19,33H3.37A3.38,3.38,0,0,1,0,29.58V17.94a3.36,3.36,0,0,1,3.37-3.37Zm36.27,0V8.51H39v6.06ZM49.48,49.25a3.4,3.4,0,0,1,6.8,0v51.81a3.4,3.4,0,1,1-6.8,0V49.25ZM69.59,49a3.4,3.4,0,1,1,6.78.42L73,101.27a3.4,3.4,0,0,1-6.78-.43L69.59,49Zm-40.26.42A3.39,3.39,0,1,1,36.1,49l3.41,51.8a3.39,3.39,0,1,1-6.77.43L29.33,49.46ZM92.51,33.38H13.19l7.94,83.55H84.56l8-83.55Z"/></svg>
-                            </button>   
-                        </div> 
+                            <section class="count-bouteilles">
+                                <button  class="" @click.prevent="decrement    (bouteille.id)" :disabled="bouteille.quantite < 1">-</button>
+
+                                <p class=""> {{ bouteille.quantite }}</p>
+
+                                <button  class="" @click.prevent="increment  (bouteille.id)" >+</button>
+
+                               <button  class="card-btn_delete deleteModalBtn" value="" @click.prevent="deleteBouteille(bouteille.id)" :disabled="bouteille.quantite > 1">
+                                    <IconContainer>
+                                        <template #icon>
+                                            <IconSupprimer/>
+                                        </template>
+                                    </IconContainer>
+                                </button>
+                            </section>
+
+
+
                     </section> 
                 </div>   
             </div>
@@ -135,6 +143,8 @@
 
 import useBouteille from '../../composables/bouteille'
 import useCellier from '../../composables/cellier'
+import IconContainer from "../IconContainer.vue";
+import IconSupprimer from '../icons/IconSupprimer.vue';
 import { onMounted, getCurrentInstance, watch, ref, computed } from 'vue'
 import axios from 'axios'
 
@@ -237,7 +247,11 @@ export default {
             changeNote,
             test,
         }
-    }, 
+    },
+    components: {
+    "IconContainer": IconContainer,
+    "IconSupprimer": IconSupprimer,
+}
 
  
 }
